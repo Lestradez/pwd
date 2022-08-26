2554af964db586b5559d376d665c2228bd73cac3
LZDEV <81957303+Lestradez@users.noreply.github.com>
2022-08-26 11:44:47 -0300

    Se crea estructura básica archivos

---
+++ includes/menu.php
@@ -0,0 +1,7 @@
+<div class="menu">
+    <ul>
+        <li><a href="index.php">Inicio</a></li>
+        <li><a href="#tp_1">Trabajo Práctico 1</a></li>
+        <li><a href="#tp_2">Trabajo Práctico 2</a></li>
+        <li><a href="#tp_3">Trabajo Práctico 3</a></li>
+    </ul>
