89a5e96b87a1c51e282f841e33793a314600bc59
LZDEV <81957303+Lestradez@users.noreply.github.com>
2022-08-29 06:46:09 -0300

    control de formato ej1 tp 3

--- tp_3/ejercicio_1/model/main.php
+++ tp_3/ejercicio_1/model/main.php
@@ -1,24 +1,34 @@
 <?php
 function guardarArchivo ($data){
     if ($data["error"] <= 0) {
+      $result ['resultado']='';
+      $formatos_permitidos =  array('doc','pdf');
+      $archivo = $data['name'];
+      $extension = pathinfo($archivo, PATHINFO_EXTENSION);
       $result ["nombre"] = $data['name'] ;
       $result ["tipo"] = $data['type'] ;
       $result ["tamaño"] = ($data['size']/1048576 ) ;
       $result ["carpeta temporal"] = $data['tmp_name'];
-      if($data["size"]/1048576 < 2){
-        if (!copy($data['tmp_name'],dirname(__DIR__).'/files/'.$data['name'])) {
-          $result ['resultado'] = "ERROR: no se pudo cargar el archivo ";
-        } else {
-          $result ['resultado'] = "El archivo ".$data['name']." se ha copiado con Éxito";
-          $result ['enlace'] = dirname($_SERVER['REQUEST_URI']).'/ejercicio_1/files/'.$data['name'];
+      
+      
+      if(in_array($extension, $formatos_permitidos) ) {
+        if($data["size"]/1048576 < 2 ){
+          
+          if (!copy($data['tmp_name'],dirname(__DIR__).'/files/'.$data['name'])) {
+            $result ['resultado'] = "ERROR: no se pudo cargar el archivo ";
+          } else {
+            $result ['resultado'] = "El archivo ".$data['name']." se ha copiado con Éxito";
+            $result ['enlace'] = dirname($_SERVER['REQUEST_URI']).'/ejercicio_1/files/'.$data['name'];
+          }
+        } else{
+          $result ['resultado'] = "El archivo es demasiado grande. El limite es de 2mb.";
         }
-      }
-      else{
-        $result ['resultado'] = "El archivo es demasiado grande. El limite es de 2mb";
-      }
+      } else {
+        $result ['resultado'] = 'Error formato no permitido !!';
+       }
     } else
-          $result ['resultado'] = "ERROR: no se pudo cargar el archivo. No se pudo acceder al archivo Temporal";
-
+     $result ['resultado'] = "ERROR: no se pudo cargar el archivo. No se pudo acceder al archivo Temporal.";
+      
   return $result;
   }
 ?>
