fd178e3d218215cba08bae6c3a7dd4912653b80a
LZDEV <81957303+Lestradez@users.noreply.github.com>
2022-08-29 12:05:39 -0300

    ejercicio 2 tp3 terminado

---
+++ tp_3/ejercicio_2/model/main.php
@@ -0,0 +1,30 @@
+<?php
+function guardarArchivo ($data){
+    if ($data["error"] <= 0) {
+      $result ["nombre"] = $data['name'] ;
+      $result ["tipo"] = $data['type'] ;
+      $result ["tamaño"] = ($data['size']/1048576 ) ;
+      $result ["carpeta temporal"] = $data['tmp_name'];
+      if($data["size"]/1048576 < 2){
+        if (!copy($data['tmp_name'],dirname(__DIR__).'/files/'.$data['name'])) {
+          $result ['resultado'] = "ERROR: no se pudo cargar el archivo ";
+        } else {
+          $result ['resultado'] = "El archivo ".$data['name']." se ha copiado con Éxito";
+          $result ['enlace'] = dirname($_SERVER['REQUEST_URI']).'/ejercicio_2/files/'.$data['name'];
+          $filePath = dirname(__DIR__).'/files/'.$data['name'] ;
+          $file = fopen(dirname(__DIR__).'/files/'.$data['name'],"r");
+          $result['texto'] = fread($file,filesize(dirname(__DIR__).'/files/'.$data['name']));
+          $result['cantLineas'] = count(file($filePath));
+          fclose($file);
+        }
+      }
+      else{
+        $result ['resultado'] = "El archivo es demasiado grande. El limite es de 2mb";
+      }
+    } else
+          $result ['resultado'] = "ERROR: no se pudo cargar el archivo. No se pudo acceder al archivo Temporal";
+
+
+  return $result;
+  }
+?>
---
+++ tp_3/ejercicio_2/view/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<head>
+  <link
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+    rel="stylesheet"
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+    crossorigin="anonymous"
+  />
+</head>
+
+<body>
+  <div class="container p-3">
+    <div class="border bg-light border-3 rounded p-2 mt-3 mb-4 ">
+      <h3>Ejercicio 1</h3>
+      <form id="formFile" name="formFile" class="mb-3" method="POST" action="<?php dirname( __DIR__ ) .'/controller/main.php'; ?>" enctype="multipart/form-data" >
+        <div class="mb-3">
+          <label for="formFile" class="form-label">Seleccione un archivo. Debe ser un archivo de texto plano (.txt)</label>
+          <input class="form-control" type="file" id="formFile" name="formFile" accept=".txt" required>
+        </div>
+        <input class="btn btn-primary" type="submit" value="Enviar" />
+      </form>
+    </div>
+  </div>
+</body>
