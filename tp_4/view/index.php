97819b487c34cdb3f7c3854eacc1625622659b8b
LZDEV <81957303+Lestradez@users.noreply.github.com>
2022-09-26 07:25:09 -0300

    se crean paginas de accion y se mejoran vistas

---
+++ tp_4/view/index.php
@@ -0,0 +1,20 @@
+<?php 
+include_once ("../config.php"); 
+$title="TP 4 - PWD";
+include_once ("../view/layout/head.php");
+include_once ("../view/layout/navbar.php");
+?>
+<section class="main-container">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-center">INFO AUTOS</h1>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<?php
+include_once ("../view/layout/footer.php");
+?>
