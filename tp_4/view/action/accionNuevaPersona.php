4fc9b283f627186ef1e59217c754fcad30a870b3
LZDEV <81957303+Lestradez@users.noreply.github.com>
2022-09-27 03:17:14 -0300

    .

--- tp_4/view/action/accionNuevaPersona.php
+++ tp_4/view/action/accionNuevaPersona.php
@@ -9,8 +9,15 @@ $resutadoAlta=$objPersona->alta($datos);
 ?>
 <section class="main-container p-5">
 
+<?php
+            if($resutadoAlta){
+                echo("<h2>La persona fue cargada con exito</h2>");
+            }// fin if 
+            else{
+                echo("<h2>Hubo problemas con la carga</h2>");
 
-
+            }// fin else
+            ?>
 
 </section>
 <?php
